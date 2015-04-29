f1369e36795c7bc3c06c07379c42d7535ec770ff
viljinsky <viljinsky@narod.ru>
2015-04-29 12:55:45 +0300

    29 04 2015  SQLite reports 2

--- src/ru/viljinsky/reports/ReportBuilder.java
+++ src/ru/viljinsky/reports/ReportBuilder.java
@@ -134,7 +134,7 @@ public class ReportBuilder {
     Dataset depart;
     Dataset hall_list;
     
-    private String getScheduleCell(int depart_id,int bell_id,int day_id) throws Exception{
+    private String getRoomLabel(int depart_id,int day_id,int bell_id) throws Exception{
         Recordset r = DataModule.getRecordet("select distinct room_name from room a inner join schedule b on a.id=b.room_id where day_id="+day_id+" and bell_id="+bell_id+" and depart_id="+depart_id);
         String room_label="";
         if (r.isEmpty())
@@ -142,18 +142,22 @@ public class ReportBuilder {
         else
             for (int i=0;i<r.size();i++){
                 if (!room_label.isEmpty())
-                    room_label+="&nbsp;";
+                    room_label+="<br>";
                 room_label+=r.get(i)[0].toString();
             }
-                    
+        return room_label;
+    }
+        
+    
+    private String getScheduleCell(int depart_id,int bell_id,int day_id) throws Exception{
+
+        String room_label = getRoomLabel(depart_id, day_id, bell_id);
             
         Values values = new Values();
         values.put("day_id",day_id);
         values.put("bell_id",bell_id);
-        if (hall_list.locate(values)>=0){
+        if (hall_list.locate(values)>=0)
             return "<td colspan='2' align='center'>****</td>";
-        }
-                
         
         for (int i=0;i<schedule.size();i++){
             values = schedule.getValues(i);
@@ -172,39 +176,46 @@ public class ReportBuilder {
      * @throws Exception 
      */
     public String getScheduleReport(Integer depart_id) throws Exception{
+        Integer day_id,bell_id;
+        Values values,filter;
+        String sql;
+
+        sql = "select distinct a.* from day_list a inner join shift_detail b\n" +
+                     "on a.day_no=b.day_id inner join depart d on d.shift_id=b.shift_id where d.id="+depart_id;
+        day_list= DataModule.getSQLDataset(sql);
+        day_list.open();
+        
+        sql = "select distinct a.* from bell_list a inner join shift_detail b on a.bell_id=b.bell_id \n"+
+                "inner join depart d on d.shift_id=b.shift_id where d.id="+depart_id;
+        
+        bell_list = DataModule.getSQLDataset(sql);
+        bell_list.open();
+        
+        
         StringBuilder result = new StringBuilder();
         Recordset r = DataModule.getRecordet("select label from depart where id="+depart_id);
         
-        Values filter = new Values();
+        filter = new Values();
         filter.put("depart_id", depart_id);
         schedule.open(filter);
         hall_list.open(filter);
 
-        result.append("<h3>");
-        result.append(r.getString(0));
-        result.append("</h3>");
+        result.append("<h3>")
+            .append(r.getString(0))
+            .append("</h3>");
         
-        
-        day_list = DataModule.getDataset("day_list");
-        day_list.open();
-        bell_list = DataModule.getDataset("bell_list");
-        bell_list.open();
-        Integer day_id,bell_id;
-        Values values;
-        
-
-        result.append("<table>");
-        result.append("<tr>");
-        result.append("<td>");
-        result.append("&nbsp;");
-        result.append("</td>");
+        result.append("<table>")
+            .append("<tr>")
+            .append("<td>")
+            .append("&nbsp;")
+            .append("</td>");
      
         // заголовок колонок
         for (int i=0;i<day_list.size();i++){
             values = day_list.getValues(i);
-            result.append("<th colspan='2'>");
-            result.append(values.getString("day_caption"));
-            result.append("</th>");
+            result.append("<th colspan='2'>")
+                .append(values.getString("day_caption"))
+                .append("</th>");
         }
         result.append("</tr>");
         
@@ -213,9 +224,9 @@ public class ReportBuilder {
             result.append("<tr>");
             // заголовок строк
             values = bell_list.getValues(row);
-            result.append("<th nowrap>");
-            result.append(values.getString("time_start"));
-            result.append("</th>");
+            result.append("<th nowrap>")
+                .append(values.getString("time_start"))
+                .append("</th>");
             
             // значения
             for (int col=0;col<day_list.size();col++){
@@ -258,19 +269,31 @@ public class ReportBuilder {
         return  result.toString() ;
     }
     
-    private String getScheduleCell2(int day_no,int bell_id,int depart_id) throws Exception{
+    
+    /**
+     * Возвращает ячейки subject_name room_labale
+     * @param depart_id
+     * @param day_id
+     * @param bell_id
+     * @return
+     * @throws Exception 
+     */
+    private String getScheduleCell2(int depart_id,int day_id,int bell_id) throws Exception{
         Values filter = new Values();
-        filter.put("day_id", day_no);
+        filter.put("day_id", day_id);
         filter.put("bell_id", bell_id);
         filter.put("depart_id", depart_id);
         if (hall_list.locate(filter)>=0){
-            return "<div align='center'>****</div>";
+            return "<td colspan='2' align='center'>****</td>";
         }
         schedule.open(filter);
         if (schedule.isEmpty())
-            return "&nbsp;";
+            return "<td colspan='2'>&nbsp;</td>";
+        
+        String room_label = getRoomLabel(depart_id, day_id, bell_id);
+        
         Values values = schedule.getValues(0);
-        return values.getString("subject_name");
+        return "<td>"+values.getString("subject_name")+"</td><td nowrap>"+room_label+"</td>";
     } 
     
     
@@ -283,7 +306,8 @@ public class ReportBuilder {
     public String getSchedueReport2() throws Exception{
         hall_list = DataModule.getSQLDataset(SQL_ERROR_HALL_IN_SCHEDULE);
         hall_list.open();
-        day_list = DataModule.getDataset("day_list");
+        String sql = "select distinct a.* from day_list a inner join shift_detail b on a.day_no=b.day_id inner join depart d on d.shift_id=b.shift_id";
+        day_list = DataModule.getSQLDataset(sql);
         day_list.open();
         bell_list=DataModule.getDataset("bell_list");
         bell_list.open();
@@ -313,8 +337,8 @@ public class ReportBuilder {
         // строки
         
             Values dayValues,bellValues;
-            for (int day_no=0;day_no<day_list.size();day_no++){
-                dayValues=day_list.getValues(day_no);
+            for (int col=0;col<day_list.size();col++){
+                dayValues=day_list.getValues(col);
                 
                 // заголовок  строка день
                 result.append("<tr>");
@@ -325,22 +349,17 @@ public class ReportBuilder {
                 result.append("</tr>");
                 
                 
-                for (int bell_id=0;bell_id<bell_list.size();bell_id++){
+                for (int row=0;row<bell_list.size();row++){
                     result.append("<tr>");
-                    bellValues = bell_list.getValues(bell_id);
-                    result.append("<th>");
+                    bellValues = bell_list.getValues(row);
+                    result.append("<th nowrap>");
+                    
                     result.append(bellValues.getString("time_start"));
                     result.append("</th>");
                     
                     for (int i=0;i<depart.size();i++){
                         values=depart.getValues(i);
-                        result.append("<td>");
-                        result.append(getScheduleCell2(day_no+1, bell_id+1, values.getInteger("id")));
-                        result.append("</td>");
-                        result.append("<td>");
-                        result.append("&nbsp;");
-                        result.append("</td>");
-                        
+                        result.append(getScheduleCell2(values.getInteger("id"),dayValues.getInteger("day_no"), bellValues.getInteger("bell_id")));
                     }
                     result.append("</tr>");
                     
